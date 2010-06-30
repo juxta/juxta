cb532a5272b35289057c841bd8bf10a44eb83dcc
Alexey Golovnya <alexey.golovnya@gmail.com>
2010-06-30 17:00:57 +0700

    Put jQuery 1.4.2. Pre-made base class

--- juxta.js
+++ juxta.js
@@ -1,1 +1,37 @@
+(function($){
+	$.class = function(template, data) {
+		function create(){
+			if (this.init) {
+				this.init.apply(this, arguments);
+			}
+		}
+		return create;
+	};
+})(jQuery);
 
+Juxta = $.class();
+Juxta.prototype = {
+	init: function(){
+		if (location.hash == '') {
+			location.hash = 'databases';
+		}
+		this.state = 'default';
+		setInterval(this.checkLocation, 200);
+	},
+	checkLocation: function(){
+		var hash = location.hash.replace(/#/g, '');
+		params = hash.split('/');
+		action = params.pop();
+		if (hash != Juxta.state) {
+			switch (action) {
+				case 'databases':
+					console.log('Databases');
+					break;
+				case 'processlist':
+					console.log('Processlist');
+					break;
+			}
+			Juxta.state = hash;
+		}
+	}
+};
