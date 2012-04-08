cc5c6ea15b44c334481165cfff2de45833f873d0
Alexey Golovnya <alexey.golovnya@gmail.com>
2012-04-08 12:45:42 +0700

    Juxta.Browser refactored

--- js/browser.js
+++ js/browser.js
@@ -1,78 +1,126 @@
-Juxta.Browser = $.Class(Juxta.Application, {
-	init: function(element, request) {
-		this._super(element, {header: 'Browse', closable: true, maximized: true});
-		this.grid = new Juxta.TreeGrid(this.$application.find('.grid'));
-		this.request = request;
-		$(window).bind('resize', {_this: this}, this.stretch);
-	},
-	show: function(options) {
-		this._show(options);
-		this.stretch();
-	},
-	stretch: function(event) {
-		var _this = event && event.data._this || this;
-		if (_this.$application.is(':visible')) {
-			_this.$application.find('.grid .body').height($('#applications').height() - _this.$application.find('.grid .body').position().top - _this.$statusBar.height() - 24);
-		}
-	},
-	browse: function(params) {
-		return this.requestBrowse(params);
-	},
-	requestBrowse: function(params) {
-		var query = $.extend({}, params),
-			options = {};
+/**
+ * @class Data browser
+ * @extends Juxta.Application
+ * @param {jQuery|String} element
+ * @param {Juxta.Request} request
+ */
+Juxta.Browser = function(element, request) {
 
-		this.grid.empty();
+	Juxta.Application.prototype.constructor.call(this, element, {header: 'Browse', closable: true, maximized: true});
 
-		this.show({
-			header: {title: 'Browse', name: params.browse, from: params.from}/*,
-			menu: {'Create Trigger': {click: 'return false;'}}*/
-		});
+	/**
+	 * @type {Juxta.TreeGrid}
+	 */
+	this.grid = new Juxta.TreeGrid(this.$application.find('.grid'));
 
-		this.request.send($.extend(
-			{},
-			{action: query, context: this, success: this.responseBrowse},
-			this.settings,
-			options
-		));
-	},
-	responseBrowse: function(response) {
-		var params = $.extend(
-			{},
-			response,
-			{
-				head: {},
-				row: null,
-				context: [],
-				contextMenu: [
-					{title: 'Delete', action: function() { console.log('Drop'); }},
-					{title: 'Edit', action: function() { console.log('Edit');  }}
-				]
-			}
-		);
 
-		if (response.columns) {
-			params.row = '<tr>';
-			$.each(response.columns, function(i, column) {
-				params.head[(
-					'column' +
-					(column[1] ? ' ' + column[1] : '') +
-					' ' + column[0]
-				).toLowerCase()] = column[0];
-				//
-				params.row += '<td class="column ' + column[0] + '"><div>{' + column[0] + '}</div></td>';
-				//params.row += '<td>{' + column[0] + '}</td>';
-				//
-				params.context.push(column[0]);
-			});
-			params.row += '</tr>';
-		}
-		delete params.data;
-		console.log(response.data);
+	/**
+	 * @type {Juxta.Request}
+	 */
+	this.request = request;
 
-		this.grid.prepare(params);
-		this.grid.fill(response.data, params);
+	$(window).bind('resize', {that: this}, this.stretch);
+}
 
-		this.show();
+Juxta.Lib.extend(Juxta.Browser, Juxta.Application);
+
+/**
+ * Show explorer
+ * @param {Object} options
+ * @retrun {Juxta.Browser}
+ */
+Juxta.Browser.prototype.show = function(options) {
+	Juxta.Application.prototype.show.apply(this, arguments);
+	this.stretch();
+
+	return this;
+}
+
+
+/**
+ * Stretch grid to window height
+ * @param {Event} event
+ */
+Juxta.Browser.prototype.stretch = function(event) {
+	var that = event && event.data.that || this;
+	if (that.$application.is(':visible')) {
+		that.$application.find('.grid .body').height($('#applications').height() - that.$application.find('.grid .body').position().top - that.$statusBar.height() - 24);
 	}
-});
+}
+
+
+/**
+ * Browse a table
+ * @param {Object} params
+ */
+Juxta.Browser.prototype.browse = function(params) {
+	return this.requestBrowse(params);
+}
+
+
+/**
+ * Request data
+ * @param {Object} params
+ */
+Juxta.Browser.prototype.requestBrowse = function(params) {
+	var query = $.extend({}, params),
+		options = {};
+
+	this.grid.empty();
+
+	this.show({
+		header: {title: 'Browse', name: params.browse, from: params.from}/*,
+		menu: {'Create Trigger': {click: 'return false;'}}*/
+	});
+
+	this.request.send($.extend(
+		{},
+		{action: query, context: this, success: this.responseBrowse},
+		this.settings,
+		options
+	));
+}
+
+
+/**
+ * Response
+ * @param {Object} response
+ */
+Juxta.Browser.prototype.responseBrowse = function(response) {
+	var params = $.extend(
+		{},
+		response,
+		{
+			head: {},
+			row: null,
+			context: [],
+			contextMenu: [
+				{title: 'Delete', action: function() { console.log('Drop'); }},
+				{title: 'Edit', action: function() { console.log('Edit');  }}
+			]
+		}
+	);
+
+	if (response.columns) {
+		params.row = '<tr>';
+		$.each(response.columns, function(i, column) {
+			params.head[(
+				'column' +
+				(column[1] ? ' ' + column[1] : '') +
+				' ' + column[0]
+			).toLowerCase()] = column[0];
+			//
+			params.row += '<td class="column ' + column[0] + '"><div>{' + column[0] + '}</div></td>';
+			//params.row += '<td>{' + column[0] + '}</td>';
+			//
+			params.context.push(column[0]);
+		});
+		params.row += '</tr>';
+	}
+	delete params.data;
+
+	this.grid.prepare(params);
+	this.grid.fill(response.data, params);
+
+	this.show();
+}
