407ad5bd21808736f01744a4b41d5eb9f61aa8e7
Alexey Golovnya <alexey.golovnya@gmail.com>
2012-04-09 22:56:33 +0700

    Juxta.TableEditor refactored

--- js/tableeditor.js
+++ js/tableeditor.js
@@ -1,21 +1,34 @@
-Juxta.TableEditor = $.Class(Juxta.Application, {
-	init: function(element) {
-		this._super(element, {closable: false, mazimized: false, menu: {'Browse table' : {click: "Jux.browse({browse: '123', from: 'Qq'}); return false;"}}});
-		$(window).bind('resize', {_this: this}, this.stretch);
-	},
-	show: function(options) {
-		this._show(options);
-		this.stretch();
-	},
-	stretch: function(event) {
-		var _this = event && event.data._this || this;
-		if (_this.$application.is(':visible')) {
-			if ($('#applications').height() < 500) {
-				_this.$application.find('.grid.indexes .body').height(72);
-			} else{
-				_this.$application.find('.grid.indexes .body').height($('#applications').height() * 0.225);
-			}
-			_this.$application.find('.grid.columns .body').height($('#applications').height() - _this.$application.find('.grid.columns .body').position().top - _this.$statusBar.height() - 24 - _this.$application.find('.grid.indexes')[0].offsetHeight - 54 /* H2 height with margins */);
+Juxta.TableEditor = function(element) {
+
+	Juxta.Application.prototype.constructor.call(this, element, {closable: false, mazimized: false, menu: {'Browse table' : {click: "Jux.browse({browse: '123', from: 'Qq'}); return false;"}}});
+
+	$(window).bind('resize', {that: this}, this.stretch);
+}
+
+Juxta.Lib.extend(Juxta.TableEditor, Juxta.Application);
+
+/**
+ * 
+ */
+Juxta.TableEditor.prototype.show = function(options) {
+	Juxta.Application.prototype.show.apply(this, arguments);
+	this.stretch();
+
+	return this;
+}
+
+
+/**
+ * 
+ */
+Juxta.TableEditor.prototype.stretch = function(event) {
+	var that = event && event.data.that || this;
+	if (that.$application.is(':visible')) {
+		if ($('#applications').height() < 500) {
+			that.$application.find('.grid.indexes .body').height(72);
+		} else{
+			that.$application.find('.grid.indexes .body').height($('#applications').height() * 0.225);
 		}
+		that.$application.find('.grid.columns .body').height($('#applications').height() - that.$application.find('.grid.columns .body').position().top - that.$statusBar.height() - 24 - that.$application.find('.grid.indexes')[0].offsetHeight - 54 /* H2 height with margins */);
 	}
-});
+}
