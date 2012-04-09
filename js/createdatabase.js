496845a484d833be2d4bde8ba0aa0a0e9343f53c
Alexey Golovnya <alexey.golovnya@gmail.com>
2012-04-09 22:46:26 +0700

    Juxta.CreateDatabase refactored

--- js/createdatabase.js
+++ js/createdatabase.js
@@ -1,64 +1,123 @@
-Juxta.CreateDatabase = $.Class(Juxta.FloatBox, {
-	init: function(element, request) {
-		this._super(element);
+/**
+ * @class Create Database
+ * @extends Juxta.FloatBox
+ * @constructor
+ * @param {jQuery} element
+ * @param {Object} options
+ */
+Juxta.CreateDatabase = function(element, request) {
 
-		this.$form = this.$floatBox.find('form[name=create-database]');
-		this.$collations = this.$form.find('[name=collation]');
-		this.$submit = this.$form.find('input[type=submit]');
-		this.request = request;
+	Juxta.Auth.prototype.constructor.call(this, element);
 
-		var that = this;
-		this.$form.bind('submit', function() {
-			that.requestCreateDatabase();
-			return false;
+	/**
+	 * @type {Juxta.Request}
+	 */
+	this.request = request;
+
+
+	/**
+	 * @type {jQuery}
+	 */
+	this.$form = this.$floatBox.find('form[name=create-database]');
+	
+	
+	/**
+	 * @type {jQuery}
+	 */
+	this.$collations = this.$form.find('[name=collation]');
+	
+	
+	/**
+	 * @type {jQuery}
+	 */
+	this.$submit = this.$form.find('input[type=submit]');
+
+	var that = this;
+	this.$form.bind('submit', function() {
+		that.requestCreateDatabase();
+		return false;
+	});
+}
+
+Juxta.Lib.extend(Juxta.CreateDatabase, Juxta.FloatBox);
+
+/**
+ * @param {Object} options
+ */
+Juxta.CreateDatabase.prototype.show = function(options) {
+	//this.requestGetCollations();
+	Juxta.FloatBox.prototype.show.apply(this, arguments);
+	this.$submit.attr('disabled', false);
+	this.$floatBox.find('input[type=text]').focus().val(null);
+}
+
+
+/**
+ * Hide window
+ */
+Juxta.CreateDatabase.prototype.hide = function() {
+	// @todo Remove global
+	Jux.notification.hide();
+	this._hide();
+}
+
+
+/**
+ * Request
+ */
+Juxta.CreateDatabase.prototype.requestCreateDatabase = function() {
+	this.$submit.attr('disabled', true);
+	this.request.send({
+		action: {create: 'database'},
+		data: this.$form.serialize(),
+		success: this.responseCreateDatabase,
+		error: function() { this.$submit.attr('disabled', false); },
+		context: this
+	});
+}
+
+
+/**
+ * Response
+ * @param {Object} response
+ */
+Juxta.CreateDatabase.prototype.responseCreateDatabase = function(response) {
+	this.$floatBox.hide();
+	// @todo Remove global
+	Jux.explore({show: 'databases', refresh: true});
+}
+
+
+/**
+ * Get collations request
+ */
+Juxta.CreateDatabase.prototype.requestGetCollations = function() {
+	this.request.send({
+		action: {show: 'collations'},
+		cache: true,
+		context: this,
+		success: this.responseGetCollations
+	});
+}
+
+
+/**
+ * Response for get collations request
+ * @param {Object} response
+ */
+Juxta.CreateDatabase.prototype.responseGetCollations = function(response) {
+	var that = this;
+	if (!$.isEmptyObject(response.data)) {
+		var $collations = this.$collations;
+		this.$collations.empty().append('<option>Default</option>');
+		$.each(response.data, function(charset, collations) {
+			var $charset = $('<optgroup>').attr('label', charset);
+			if (!$.isEmptyObject(collations)) {
+				$.each(collations, function(index, charset) {
+					$charset.append($('<option>').text(charset));
+				});
+			}
+			that.$collations.append($charset);
 		});
-	},
-	show: function(options) {
-		//this.requestGetCollations();
-		this._show(options);
-		this.$submit.attr('disabled', false);
-		this.$floatBox.find('input[type=text]').focus().val(null);
-	},
-	hide: function() {
-		Jux.notification.hide();
-		this._hide();
-	},
-	requestCreateDatabase: function() {
-		this.$submit.attr('disabled', true);
-		this.request.send({
-			action: {create: 'database'},
-			data: this.$form.serialize(),
-			success: this.responseCreateDatabase,
-			error: function() { this.$submit.attr('disabled', false); },
-			context: this
-		});
-	},
-	responseCreateDatabase: function(response) {
-		this.$floatBox.hide();
-		Jux.explore({show: 'databases', refresh: true});
-	},
-	requestGetCollations: function() {
-		this.request.send({
-			action: {show: 'collations'},
-			cache: true,
-			context: this,
-			success: this.responseGetCollations
-		});
-	},
-	responseGetCollations: function(response) {
-		var that = this;
-		if (!$.isEmptyObject(response.data)) {
-			var $collations = this.$collations;
-			this.$collations.empty().append('<option>Default</option>');
-			$.each(response.data, function(charset, collations) {
-				var $charset = $('<optgroup>').attr('label', charset);
-				if (!$.isEmptyObject(collations)) {
-					$.each(collations, function(index, charset) {
-						$charset.append($('<option>').text(charset));
-					});
-				}
-				that.$collations.append($charset);
-			});
-		}
 	}
-});
+}
