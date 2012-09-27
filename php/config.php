4f8967486419c5c28c3bed2c572d75b75cbd5e74
Alexey Golovnya <alexey.golovnya@gmail.com>
2012-09-27 22:10:52 +0700

    Change stored connections config

--- php/config.php
+++ php/config.php
@@ -2,8 +2,6 @@
 
 $config = array(
 	'stored_connections' => array(
-		array('host' => '127.0.0.1', 'port' => '3306',
-			  'user' => 'root', 'password' => '31337'),
-		array('host' => 'localhost', 'user' => 'admin')
+		array('host' => 'localhost', 'user' => 'root')
 	)
 );
