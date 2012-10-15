497515532213a030e66ab263524bea0ce258ae21
Alexey Golovnya <alexey.golovnya@gmail.com>
2012-10-15 20:54:36 +0700

    Replace SQL editor to Codemirror

--- js/sqleditor.js
+++ js/sqleditor.js
@@ -6,80 +6,54 @@
 Juxta.SqlEditor = function(textarea, options) {
 
 	/**
-	 * @type {jQuery}
+	 * @type {Object}
+	 * @private
 	 */
-	this.textarea = $(textarea);
+	this._settings = {
+		mode: "text/x-mysql",
+		tabMode: "indent",
+		tabSize: 4,
+		matchBrackets: true,
+		lineNumbers: true,
+		fixedGutter: true,
+		autoFormatLineBreaks: true
+	};
+
+	$.extend(this._settings, options);
 
 	/**
 	 * @type {jQuery}
+	 * @private
 	 */
-	this.numbers = this.textarea.before('<ul class="line-numbers"><li>1</li></ul>').prev('ul');
+	this._textarea = $(textarea);
+
 
 	/**
-	 * @type {Number}
+	 * @type {CodeMirror}
+	 * @private
 	 */
-	this.lines = 1;
+	this._editor = CodeMirror.fromTextArea(this._textarea.get(0), this._settings);
 
-	this.numbers.css('height', this.textarea.prop('clientHeight'));
-
-	var that = this;
-
-	this.textarea.resize(function() {
-		// Resize line numbers container on text area resize
-		that.numbers.css('height', this.clientHeight);
-	}).scroll(function() {
-		// Scroll line numbers with text area
-		that.numbers.find('li:first-child').css({'margin-top': -this.scrollTop + 'px'});
-	});
-
-	this.textarea.keydown(function(event) {
-		if (event.which == 13) {	// Scroll to the left when new line starts
-			this.scrollLeft = 0; 
-		} else if (event.keyCode == 9 && !event.shiftKey && !event.altKey) {
-			var start = this.selectionStart;
-			var end = this.selectionEnd;
-
-			this.value = this.value.substring(0,start) + "\t" + this.value.substring(end, this.value.length);
-
-			this.selectionStart = start+1;
-			this.selectionEnd = start+1;
-
-			return false;
-		}
-
-	});
-
-	/*
-	 * @type {Number}
-	 */
-	var textAreaHeight = this.textarea.prop('clientHeight');
-
-	// Calculating line numbers
-	var t = setInterval(function() {
-		rows = that.textarea.attr('value').replace(/\r\n/g, '\n').replace(/\n\r/g, '\n').split('\n').length;
-		if (rows != that.lines) {
-			if (rows > that.lines) {
-				for (row = that.lines + 1; row <= rows; row++) {
-					that.numbers.append('<li>' + row + '</li>');
-					that.lines++;
-				}
-			} else if (rows < that.lines) {
-				that.numbers.find('li').slice(rows - that.lines).remove();
-				that.lines = rows;
-			}
-		}
-
-		if (that.textarea.prop('clientHeight') != textAreaHeight) {
-			that.textarea.trigger('resize');
-			textAreaHeight = that.textarea.prop('clientHeight');
-		}
-	}, 100);
 
 	/**
+	 * Set the editor content
 	 * @param {String} text
+	 * @return {Juxta.SqlEditor}
 	 */
 	this.edit = function(text) {
-		this.textarea.text(text);
+		//
+		this._editor.setValue(text);
+		return this;
 	}
 
-};
+
+	/**
+	 * Redraw the editor
+	 * @return {Juxta.SqlEditor}
+	 */
+	this.refresh = function() {
+		this._editor.refresh();
+		return this;
+	}
+
+}
